2b226c6c6eb09ba92a6bb2c0276acf60a134ed9c
benderillo <alex.veprik@gmail.com>
2012-03-27 21:23:15 +0900

    Update Awful_logger.cpp

--- Awful_logger.cpp
+++ Awful_logger.cpp
@@ -5,15 +5,11 @@
     General Description: Logging utility functionality is defined here.
 
 ====================================================================================================
-                               Chaotic Systems Confidential Restricted
-                        (c) Copyright Chaotic Systems 2008 All Rights Reserved
-
-
 Revision History:
                          Modification
 Author                       Date          Major Changes
 ----------------------   ------------      -------------
-Alexander Veprik         09/01/2008        Initial version
+Anonymous                09/01/2008        Initial version
 
 ==================================================================================================*/
 //=================================================================================================
